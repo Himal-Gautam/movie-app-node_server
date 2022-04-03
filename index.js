1e4fe06b917d142fd64827ff2f81805ff93d0c35
Himal Gautam <himalpgautam@gmail.com>
2022-04-03 12:04:12 +0530

    .

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { MongoClient } from "mongodb";
 import cors from "cors";
-import {moviesRouter} from "./routes/movies.js";
+// import {moviesRouter} from "./routes/movies.js";
 
 
 dotenv.config();
@@ -22,68 +22,68 @@ createConnection();
 
 app.use(cors());
 app.use(express.json());
-app.use('/movies', moviesRouter);
+// app.use('/movies', moviesRouter);
 
 
 app.get("/", function (request, response) {
   response.send("Hello World");
 });
 
-// app.get("/movies", async function (request, response) {
-//   const movies = await client
-//     .db("database1")
-//     .collection("movies")
-//     .find({})
-//     .toArray();
-//   await response.send(movies);
-// });
+app.get("/movies", async function (request, response) {
+  const movies = await client
+    .db("database1")
+    .collection("movies")
+    .find({})
+    .toArray();
+  await response.send(movies);
+});
 
-// app.get("/movies/:id", async function (request, response) {
-//   console.log(request.params);
-//   const { id } = request.params;
-//   const movie = await client
-//     .db("database1")
-//     .collection("movies")
-//     .findOne({ id });
-//   movie
-//     ? response.send(movie)
-//     : response.status(404).send({ message: "No such movie found 😅" });
-// });
+app.get("/movies/:id", async function (request, response) {
+  console.log(request.params);
+  const { id } = request.params;
+  const movie = await client
+    .db("database1")
+    .collection("movies")
+    .findOne({ id });
+  movie
+    ? response.send(movie)
+    : response.status(404).send({ message: "No such movie found 😅" });
+});
 
-// app.delete("/movies/:id", async function (request, response) {
-//   console.log("request.params", request.params);
-//   const { id } = request.params;
-//   const result = await client
-//     .db("database1")
-//     .collection("movies")
-//     .deleteOne({ id: id });
-//   console.log(result);
-//   response.send(result);
-// });
+app.delete("/movies/:id", async function (request, response) {
+  console.log("request.params", request.params);
+  const { id } = request.params;
+  const result = await client
+    .db("database1")
+    .collection("movies")
+    .deleteOne({ id: id });
+  console.log(result);
+  response.send(result);
+});
 
-// app.put("/movies/:id", async function (request, response) {
-//   console.log("request.params", request.params);
-//   const { id } = request.params;
-//   const updateData = request.body;
-//   const result = await client
-//     .db("database1")
-//     .collection("movies")
-//     .updateOne({ id: id }, { $set: updateData });
-//   console.log(result);
+app.put("/movies/:id", async function (request, response) {
+  console.log("request.params", request.params);
+  const { id } = request.params;
+  const updateData = request.body;
+  const result = await client
+    .db("database1")
+    .collection("movies")
+    .updateOne({ id: id }, { $set: updateData });
+  console.log(result);
 
-//   response.send(result);
-// });
+  response.send(result);
+});
 
-// app.post("/movies", async function (request, response) {
-//   const newMovies = request.body;
-//   console.log(newMovies);
-//   // db.movies.insertMany(data)
-//   const result = await client
-//     .db("database1")
-//     .collection("movies")
-//     .insertMany(newMovies);
+app.post("/movies", async function (request, response) {
+  const newMovies = request.body;
+  console.log(newMovies);
+  // db.movies.insertMany(data)
+  const result = await client
+    .db("database1")
+    .collection("movies")
+    .insertMany(newMovies);
 
-//   response.send(result);
-// });
+  response.send(result);
+});
 
 app.listen(PORT, () => console.log(`Server is started in ${PORT}`));
